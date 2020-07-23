1a78162f6753730b634ae7f537aece8d2c440749
sun yong <successmail@126.com>
2020-07-23 22:52:05 +0800

    second

--- main.py
+++ main.py
@@ -2,14 +2,15 @@
 import sys
 import time
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QPushButton, QAbstractItemView, QDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QPushButton, QAbstractItemView, QDialog, \
+    QMessageBox, QWidget
 from functools import partial
 
 
 import second
 import third
 import Warning
-from PyQt5.QtCore import QTimer, QThread, pyqtSignal
+from PyQt5.QtCore import QTimer, QThread, pyqtSignal, Qt
 
 from PyQt5.QtGui import QBrush, QColor
 
@@ -19,8 +20,12 @@ from qtpy import QtCore
 
 def click_success():
     print("啊哈哈哈我终于成功了！")
-def convert(ui):
-    partial
+def showTaskDialog():
+    dialog=QDialog
+    # btn=QPushButton("ok",dialog)
+    # btn.move(50,50)
+    dialog.setWindowModality(Qt.ApplicationModal)
+    dialog.exec_()
 def times(data):
     ui.label_8.setText('{} s'.format(time.strftime("%Y-%m-%d %H:%M:%S")))
 
@@ -45,6 +50,7 @@ def show_dialog(data):
 class Threads(QThread):
     update_date = pyqtSignal(str) # pyqt5 支持python3的str，没有Qstring
     warning_dialog=pyqtSignal(str)
+    task_warning_dialog=pyqtSignal(str)
     def  __init__(self):
         super(Threads,self).__init__()
     def run(self):
@@ -56,8 +62,11 @@ class Threads(QThread):
             # times()
             # print(a)
             a=a+1
-            if a>=10:
+            if a>=100:
                 self.warning_dialog.emit(str(a))
+            elif a>=4:
+                self.task_warning_dialog.emit(str(a))
+
             self.sleep(1)
 def set_UI(ui):
     ui.tableWidget.setColumnCount(10)
@@ -82,13 +91,14 @@ if __name__ == '__main__':
     set_UI(ui)
     child: childWindow = childWindow()
     #通过toolButton将两个窗体关联
-    btn = ui.pushButton
-    btn.clicked.connect(child.show)
+    # btn = ui.pushButton
+    # btn.clicked.connect(child.show)
 
     MainWindow.show()
     threadss = Threads()
     threadss.update_date.connect(times)
     threadss.warning_dialog.connect(show_dialog)# 链接信号
+    threadss.task_warning_dialog.connect(show_dialog)# 链接信号
 
     threadss.start()
     sys.exit(app.exec_())
